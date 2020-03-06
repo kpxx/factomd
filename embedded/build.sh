e5ba5601a1750bec835594163f7d8b9c3ba6dc61
stackdump <myork@stackdump.com>
2020-03-06 16:43:19 -0600

    new wax based library

---
+++ embedded/build.sh
@@ -0,0 +1,1 @@
+go build -v -o factomd.so -buildmode=c-shared factomd.go
---
+++ embedded/factomd.go
@@ -0,0 +1,47 @@
+// Copyright 2017 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package main
+
+import "C"
+import (
+	"fmt"
+	"github.com/FactomProject/factomd/common/constants/runstate"
+	"github.com/FactomProject/factomd/fnode"
+	"github.com/FactomProject/factomd/testHelper/simulation"
+	"github.com/FactomProject/factomd/worker"
+	"time"
+)
+
+//export Serve
+func Serve() {
+	// use standard ports but in-mem db
+	CmdLineOptions := map[string]string{
+		"--db":               "Map",
+		"--network":          "LOCAL",
+		"--net":              "alot+",
+		"--logPort":          "6060", // pprof port
+		"--port":             "8088",
+		"--controlpanelport": "8090", // NOTE: control panel disabled
+		"--networkport":      "8110",
+		"--debuglog":         "",
+	}
+	simulation.StartSim(1, CmdLineOptions)
+
+	s := fnode.GetFnodes()[0].State
+	for s.GetRunState() != runstate.Stopped {
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Waiting to Shut Down") // This may not be necessary anymore with the new run state method
+	time.Sleep(time.Second * 5)
+}
+
+//export Shutdown
+func Shutdown() {
+	worker.SendSigInt()
+}
+
+func main() {
+	Serve()
+}
